b56d40a0f2fd3204dd4d97684f0da8afdd71bb9c
Stefan Jeglorz <S.Jeglorz@devware.de>
2024-08-02 16:30:33 +0200

    added blazor wasm project

--- Practise.AppHost/Program.cs
+++ Practise.AppHost/Program.cs
@@ -6,6 +6,10 @@ var sqlDatabase = sqlServer.AddDatabase("exampledb");
 var weatherAPI = builder.AddProject<Projects.WeatherAPI>("WeatherAPI");
 var contactAPI = builder.AddProject<Projects.ContactAPI>("ContactAPI")
                         .WithReference(sqlDatabase)
-                        .WithReplicas(10);
+                        .WithReplicas(2);
+
+builder.AddProject<Projects.Blazor_WASM_App>("BlazorWebAssemplyApp")
+       .WithReference(weatherAPI)
+       .WithReference(contactAPI);
 
 builder.Build().Run();
